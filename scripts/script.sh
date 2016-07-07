cde3342cebf6733334f564ceb6ab0224fee72dda
Marcel Sinn <loopyargon@gmail.com>
2016-07-07 10:30:54 +0200

    Init

---
+++ scripts/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo -e "
+\n
+       --> Welcome to docker asciinema <--
+\nFeel free to contact me: <loopyargon@gmail.com>. 
+            Greetings ~ArgonQQ\n" 
+echo -e "
+$COLOR_GREEN$(/usr/bin/asciinema auth)
+\n$COLOR_OFF"
+bash
